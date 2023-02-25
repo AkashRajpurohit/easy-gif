efcc523bc4a4c9c06b4351dd33b34b422a2fb026
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2023-02-25 15:27:37 +0530

    feat: :sparkles: added cache control

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { cache } from 'hono/cache';
 import { getGifByText } from './lib/tenor';
 
 type Bindings = {
@@ -7,24 +8,31 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/:text', async (c) => {
-  const text = c.req.param('text');
-  const apiKey = c.env.TENOR_API_KEY;
+app.get(
+  '/:text',
+  cache({
+    cacheName: 'easy-gif',
+    cacheControl: 'max-age=1296000, s-maxage=1296000', // cache for 15 days
+  }),
+  async (c) => {
+    const text = c.req.param('text');
+    const apiKey = c.env.TENOR_API_KEY;
 
-  const { error, url } = await getGifByText({ apiKey, text });
+    const { error, url } = await getGifByText({ apiKey, text });
 
-  if (error || !url) {
-    return c.json({ error }, 400);
+    if (error || !url) {
+      return c.json({ error }, 400);
+    }
+
+    // Fetch the GIF from url and stream the response
+    const imageResponse = await fetch(url);
+
+    const { readable, writable } = new TransformStream();
+
+    imageResponse.body?.pipeTo(writable);
+
+    return c.newResponse(readable, 200);
   }
-
-  // Fetch the GIF from url and stream the response
-  const imageResponse = await fetch(url);
-
-  const { readable, writable } = new TransformStream();
-
-  imageResponse.body?.pipeTo(writable);
-
-  return c.newResponse(readable, 200);
-});
+);
 
 export default app;
